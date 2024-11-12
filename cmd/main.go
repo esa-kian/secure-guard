8c2c154f8f2e7ea7c56dd5193193738392e17bf4
Esa <ehsan_amd72@hotmail.com>
2024-11-12 19:54:26 +0200

    Implement the Basic HTTP Server

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/esa-kian/secureguard/internal/firewall"
+)
+
+// Basic request handler function
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	// Process incoming request with firewall rules
+	if firewall.CheckRequest(r) {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("403 - Forbidden"))
+		return
+	}
+
+	// Allow request if it passes firewall rules
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Welcome to SecureGuard!"))
+}
+
+func main() {
+	// Set up the HTTP server and route
+	http.HandleFunc("/", requestHandler)
+
+	fmt.Println("Starting SecureGuard server on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
